4b7895ac88a809d71a0e35750407541898bc0b12
aiko <zichupan26@gmail.com>
2025-10-23 07:43:54 -0500

    main.rs code complete

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use std::env;
 use std::sync::atomic::Ordering;
 
 pub mod lab2;
-
 use lab2::declarations::{MIN_ARGS, MAX_ARGS, PROGRAM_NAME_INDEX, CONFIG_FILE_INDEX, 
-                         VERBOSE_FLAG_INDEX, BAD_COMMAND_LINE_ERROR, WHINGE_MODE, SCRIPT_GENERATION_ERROR};
+                         VERBOSE_FLAG_INDEX, BAD_COMMAND_LINE_ERROR, WHINGE_MODE, SCRIPT_GENERATION_ERROR, SUCCESS};
 use lab2::play::Play;
 use lab2::return_wrapper::ReturnWrapper;
 
@@ -12,7 +11,7 @@ fn usage(program_name: &String) {
     println!("usage: {} <script_file_name> [whinge]", program_name);
 }
 
-fn parse_args(script_file: &mut String) -> Result<(), u8> {
+fn parse_args(script_filename: &mut String) -> Result<(), u8> {
     let mut args: Vec<String> = Vec::new();
     for arg in env::args() {
         args.push(arg);
@@ -28,7 +27,7 @@ fn parse_args(script_file: &mut String) -> Result<(), u8> {
         return Err(BAD_COMMAND_LINE_ERROR);
     }
 
-    *script_file = args[CONFIG_FILE_INDEX].clone();
+    *script_filename = args[CONFIG_FILE_INDEX].clone();
 
     if args.len() == MAX_ARGS && args[VERBOSE_FLAG_INDEX] == "whinge" {
         WHINGE_MODE.store(true, Ordering::SeqCst);
@@ -38,19 +37,19 @@ fn parse_args(script_file: &mut String) -> Result<(), u8> {
 }
     
 fn main() -> ReturnWrapper {
-    let mut script_file_name = String::new();
+    let mut script_filename = String::new();
 
-    if let Err(error_code) = parse_args(&mut script_file_name) {
+    if let Err(error_code) = parse_args(&mut script_filename) {
         return ReturnWrapper::new(error_code);
     }
 
     let mut play = Play::new();
 
-    if let Err(_error_code) = play.prepare(&script_file_name) {
-        return ReturnWrapper::new(SCRIPT_GENERATION_ERROR);
+    if let Err(error_code) = play.prepare(&script_filename) {
+        return ReturnWrapper::new(error_code);
     }
 
     play.recite();
 
-    ReturnWrapper::new(0)
+    ReturnWrapper::new(SUCCESS)
 }
